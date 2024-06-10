5278179102ec0ef4d647410ddaea4229352dc04c
FraisVI <FraisVI@yandex.ru>
2024-06-10 18:06:30 +0700

    Delete token and subdomain

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -32,12 +32,12 @@ class FormController extends Controller
     private function authorizationAmoCrm(): AmoCRMApiClient
     {
 
-        $accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ1NWQxNDkwN2JkMWMzYjJiMDkzMWMwMDk4MzAwODcyOTI0ZDQ0MTMxYTA1ZWM2NDc1Nzg2NWM2MjhjZjcxYzcyYjU5OTJjODQ5NmU1MTE0In0.eyJhdWQiOiJmN2NjZWQwMC02OGY4LTQyODgtOWUwNy02MjY4OTIyYWUzMDYiLCJqdGkiOiI0NTVkMTQ5MDdiZDFjM2IyYjA5MzFjMDA5ODMwMDg3MjkyNGQ0NDEzMWEwNWVjNjQ3NTc4NjVjNjI4Y2Y3MWM3MmI1OTkyYzg0OTZlNTExNCIsImlhdCI6MTcxNzc3NDU0MSwibmJmIjoxNzE3Nzc0NTQxLCJleHAiOjE3MTk3OTIwMDAsInN1YiI6IjExMTI4MDE4IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNzg2MzgyLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiY2NlNThmYWItMDVlZi00Zjg3LWEzY2UtMGMzZGNhYTllYWRjIn0.FG35glMF2YqZCFKHt7Ogp2JJYpESC2b3k8mmx1yhlqpWSYsCbLAQCuq5FWoirNEapyT2fENejyBJkkf3d7qdtm_Vpi6pNEofZ1NicnutC1xdm_RXFfV4FXRxrYNqXMkh8MYYTKyH1F6j7DfXSLY6eZdROpxn2f3n7mAcfyov6X0seeSGx7w1X8-Wum2tR94MHTvNBLvgfPzVOfU_Y3DdV4NBqPOYH8x39rLKdfL8OQsZJjGgWLMBj5V8YYN6Xm2Bgxjgj2py8RG7vt9f0isKYgg4RoQ6p4Ih_yZ2V94NO6U__IwC2pKODXFAMtHYlo7OiD-9r1wb3W4c359tkTamXA';
+        $accessToken = 'longLiveToken';
         $apiClient = new AmoCRMApiClient();
         $longLivedAccessToken = new LongLivedAccessToken($accessToken);
 
         $apiClient->setAccessToken($longLivedAccessToken)
-            ->setAccountBaseDomain('fraisvii.amocrm.ru');
+            ->setAccountBaseDomain('subdomain.amocrm.ru');
 
         return $apiClient;
     }
